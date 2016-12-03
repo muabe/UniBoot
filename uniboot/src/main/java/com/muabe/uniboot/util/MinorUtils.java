a14b70c7413849ac6554dc26e3d4dfb0727df183
MarkJ <markjmind@gmail.com>
2016-12-04 03:22:31 +0900

    MinorUtils 추가

---
+++ uniboot/src/main/java/com/muabe/uniboot/util/MinorUtils.java
@@ -0,0 +1,33 @@
+package com.muabe.uniboot.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+
+/**
+ * Created by MarkJ on 2016-12-04.
+ */
+
+public class MinorUtils {
+
+    public static String StreamToString(InputStream in) throws IOException {
+        if(in == null) {
+            return "";
+        }
+        Writer writer = new StringWriter();
+        char[] buffer = new char[1024];
+        try {
+            Reader reader = new BufferedReader(new InputStreamReader(in, "UTF-8"));
+            int n;
+            while ((n = reader.read(buffer)) != -1) {
+                writer.write(buffer, 0, n);
+            }
+        } finally {
+        }
+        return writer.toString();
+    }
+}
