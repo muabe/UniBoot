8c5f548d23e925fe0f878a5da4245c0d6f0f6b0e
Muabe <markjmind@gmail.com>
2018-07-05 18:51:10 +0900

    컴마 유틸 추가

--- uniboot/src/main/java/com/muabe/uniboot/util/JwTextUtils.java
+++ uniboot/src/main/java/com/muabe/uniboot/util/JwTextUtils.java
@@ -1,63 +1,82 @@
 package com.muabe.uniboot.util;
 
-public class JwTextUtils{
+import java.util.Locale;
 
-	public static String optEmpty(String str, String emptyString){
-		if(str==null || str.length()==0){
+public class JwTextUtils {
+
+	public static String optEmpty(String str, String emptyString) {
+		if (str == null || str.length() == 0) {
 			return emptyString;
 		}
-		return str;	
+		return str;
 	}
-	
-	public static String optNull(String str, String nullString){
-		if(str==null){
+
+	public static String optNull(String str, String nullString) {
+		if (str == null) {
 			return nullString;
 		}
-		return str;	
+		return str;
 	}
-	
-	public static boolean isContains(String str, String[] array){
-		if(str!=null){
-			for(int i=0;i<array.length;i++){
-				if(str.equals(array[i])){
+
+	public static boolean isContains(String str, String[] array) {
+		if (str != null) {
+			for (int i = 0; i < array.length; i++) {
+				if (str.equals(array[i])) {
 					return true;
 				}
 			}
 		}
 		return false;
 	}
-	
 
-	public static String getContains(String str, String[] array){
-		if(str!=null){
-			for(int i=0;i<array.length;i++){
-				if(str.equals(array[i])){
+
+	public static String getContains(String str, String[] array) {
+		if (str != null) {
+			for (int i = 0; i < array.length; i++) {
+				if (str.equals(array[i])) {
 					return array[i];
 				}
 			}
 		}
 		return null;
 	}
-	
-	public static String getContains(String str, String[] array, String defaultString){
-		if(str!=null){
-			for(int i=0;i<array.length;i++){
-				if(str.equals(array[i])){
+
+	public static String getContains(String str, String[] array, String defaultString) {
+		if (str != null) {
+			for (int i = 0; i < array.length; i++) {
+				if (str.equals(array[i])) {
 					return array[i];
 				}
 			}
 		}
 		return defaultString;
 	}
-	
-	public static int getContainsIndex(String str, String[] array, int defaultIndex){
-		if(str!=null){
-			for(int i=0;i<array.length;i++){
-				if(str.equals(array[i])){
+
+	public static int getContainsIndex(String str, String[] array, int defaultIndex) {
+		if (str != null) {
+			for (int i = 0; i < array.length; i++) {
+				if (str.equals(array[i])) {
 					return i;
 				}
 			}
 		}
 		return defaultIndex;
 	}
+
+
+	public static String comma(Object number){
+		if(number==null){
+			return "";
+		}
+		return String.format(Locale.getDefault(), "%,d",number);
+	}
+
+	public static String comma(int number){
+		return String.format(Locale.getDefault(), "%,d",number);
+	}
+
+	public static String comma(long number){
+		return String.format(Locale.getDefault(), "%,d",number);
+	}
 }
+
